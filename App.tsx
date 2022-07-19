d03ae58cfcc115b593e7de2460ab91221af82521
Lucas Porto <lucaspmv@hotmail.com>
2022-07-18 22:10:46 -0300

    config: typescript, eslint and prettier

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
+
+export default function App() {
+  return (
+    <SignIn />
+  );
+}
+
+
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function SignIn() {
+  return (
+    <View>
+      <Text>Starting...</Text>
+    </View>
+  );
+}
+
+export { SignIn };
