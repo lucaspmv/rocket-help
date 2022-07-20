a2ddce7d1a999b76937f246b37106beba42006e7
Lucas Porto <lucaspmv@hotmail.com>
2022-07-19 21:56:50 -0300

    feat: adding native base and others libs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
-import { SafeAreaView } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
   );
 }
-
-
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from "react-native";
+import { VStack, Heading } from 'native-base';
+
+import Logo from '../assets/logo_primary.svg';
 
 function SignIn() {
   return (
-    <View>
-      <Text>Starting...</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+    </VStack>
   );
 }
 
